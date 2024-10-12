bbc3dee5210d8eeee83cd448bf10e69c39f83595
matthew dalby <matthew.dalby@g3enterprises.com>
2024-10-12 03:59:41 -0700

    iniital project

---
+++ src/main/java/com/thirdgear/example/erp_api_demo/inv_tracking/InvTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.thirdgear.example.erp_api_demo.inv_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirdgear/example/erp_api_demo/inv_tracking/InvTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirdgear.example.erp_api_demo.inv_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
